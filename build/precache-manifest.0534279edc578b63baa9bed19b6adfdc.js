b241141430f581a64b42d404bc403252fa1df99a
Matthew Brody <matt@matthews-air-3.attlocal.net>
2019-12-15 20:47:42 -0600

    redeployed github pages

--- build/precache-manifest.0534279edc578b63baa9bed19b6adfdc.js
+++ build/precache-manifest.0534279edc578b63baa9bed19b6adfdc.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "543999785d3b701ea5c5",
-    "url": "/static/js/main.54399978.chunk.js"
+    "revision": "405ea64a1aee5c30fad7",
+    "url": "/static/js/main.405ea64a.chunk.js"
   },
   {
     "revision": "54094b48b410ee26b80e",
     "url": "/static/js/2.54094b48.chunk.js"
   },
   {
-    "revision": "ef596df167022af9a0e07b2c77167f7d",
+    "revision": "862b565ebc5175a7f8894691e207aeb9",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1f739ecf5effe892d5668e84afe3a2d.js"
+  "/precache-manifest.0534279edc578b63baa9bed19b6adfdc.js"
 );
 
 workbox.clientsClaim();
