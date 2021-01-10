ec37730f5588af51f2866eb659cff2e88c23cd26
mattlbrody <mattlbrody@gmail.com>
2021-01-09 22:10:33 -0600

    deployed to gh-pages

---
+++ build/precache-manifest.4454f7a7ddd12bbd95ec613c2dca8b68.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcdc760e35cc2bf644b0",
+    "url": "/static/js/main.dcdc760e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "54094b48b410ee26b80e",
+    "url": "/static/js/2.54094b48.chunk.js"
+  },
+  {
+    "revision": "30557ca1d713d75a56eeb46a002b4413",
+    "url": "/index.html"
+  }
+];
