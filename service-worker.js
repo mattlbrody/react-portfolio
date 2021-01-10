1e859e12a4e3e85022bdd63f1440bfd7c1886ef7
mattlbrody <mattlbrody@gmail.com>
2021-01-09 22:09:52 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0534279edc578b63baa9bed19b6adfdc.js"
+  "/precache-manifest.4454f7a7ddd12bbd95ec613c2dca8b68.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
